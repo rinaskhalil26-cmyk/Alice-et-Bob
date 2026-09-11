e88b0a40d1536f1bb6c950a88a0e2d336c2f365f
OUAKLI SMAIL <smail213cp@gmail.com>
2026-09-11 15:24:05 +0200

    Revert "Sub"
    This reverts commit b2713a227e5262ad180983794afed257126828da.

--- tp1git/src/main/java/iut/Sub.java
+++ tp1git/src/main/java/iut/Sub.java
@@ -1,7 +1,7 @@
 package iut;
 
 public class Sub {
-    public double sub(double a, double b){
+    public static double sub(double a, double b){
         return  a - b;
     }
 }
--- tp1git/src/test/java/iut/SubTest.java
+++
@@ -1,9 +0,0 @@
-package iut;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class SubTest {
-    @Test
-    public void test1() {
-        assertEquals(0, Sub.sub(1, 1));
-}
