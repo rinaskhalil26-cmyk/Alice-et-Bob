a02a6929b49e77363b780762652c16554d168e04
Khalil Rinas <rinaskhalil26@gmail.com>
2026-09-11 14:59:32 +0200

    Ajout de la fonction addition et des tests

--- tp1git/src/test/java/iut/AppTest.java
+++ tp1git/src/test/java/iut/AppTest.java
@@ -1,19 +1,14 @@
 package iut;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
+public class AddTest {
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testAdd() {
+        Add addition = new Add();
+        int resultat = addition.add(2, 3);
+        assertEquals(5, resultat, "2 + 3 doit être égal à 5");
     }
 }
