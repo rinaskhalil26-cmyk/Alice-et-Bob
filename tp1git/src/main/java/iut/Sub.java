b2713a227e5262ad180983794afed257126828da
OUAKLI SMAIL <smail213cp@gmail.com>
2026-09-11 15:16:42 +0200

    Sub

--- tp1git/src/main/java/iut/Sub.java
+++ tp1git/src/main/java/iut/Sub.java
@@ -1,7 +1,7 @@
 package iut;
 
 public class Sub {
-    public static double sub(double a, double b){
+    public double sub(double a, double b){
         return  a - b;
     }
 }
---
+++ tp1git/src/test/java/iut/SubTest.java
@@ -0,0 +1,9 @@
+package iut;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SubTest {
+    @Test
+    public void test1() {
+        assertEquals(0, Sub.sub(1, 1));
+}
