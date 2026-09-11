018466ddc8205098f727ff582cc1342da3842e11
Khalil Rinas <rinaskhalil26@gmail.com>
2026-09-11 15:11:12 +0200

    Ajout de la classe Add et de son test unitaire

--- tp1git/src/test/java/iut/Add.java
+++ tp1git/src/test/java/iut/Add.java
@@ -1,6 +1,8 @@
 package iut;
 
 public class Add {
+    
+    // Méthode demandée par le TP qui retourne la somme de a + b
     public int add(int a, int b) {
         return a + b;
     }
--- tp1git/src/test/java/iut/AddTest.java
+++ tp1git/src/test/java/iut/AddTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AddTest {
-    
+
     @Test
     public void testAdd() {
+        // On crée une instance de la classe Add
         Add addition = new Add();
+        
+        // On teste la méthode avec 2 et 3
         int resultat = addition.add(2, 3);
-        assertEquals(5, resultat, "2 + 3 doit être égal à 5");
+        
+        // On vérifie que le résultat est bien égal à 5
+        assertEquals(5, resultat, "Le résultat de 2 + 3 doit être 5");
     }
 }
